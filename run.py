ea9c1931f30d30bc3de93d1c96cba110b08d7400
Ngozi89 <>
2023-09-28 00:23:03 +0000

    Set scope to lists the APIs

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 import gspread
 from google_auth_oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
